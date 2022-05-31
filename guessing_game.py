b7542469c8effb1110843f07e5d29f99348faa8f
kineticvenom <kalebvarnes@yahoo.com>
2022-05-31 10:12:37 -1000

    Working Code!

--- guessing_game.py
+++ guessing_game.py
@@ -1,21 +1,26 @@
 class GuessingGame():
 
-    def __init__(self, answer, user_guess):
+    def __init__(self, answer):
         self.answer = answer
-        self.user_guess = user_guess
         
-    def guess(self):
-        if self.user_guess == self.answer:
+        
+    def guess(self, user_guess):
+        self.user_guess = user_guess
+        if user_guess == self.answer:
             return "Correct!"
-        elif self.user_guess > self.answer:
+        elif user_guess > self.answer:
             return "high!"
         else: return "low!"
-        
+    
+    
     
     def solved(self):
-        if self.guess() == self.answer:
-            print(f"Your last guess {self.user_guess} was {self.guess()}.")
-        else: print(f"Your last guess {self.user_guess} was {self.guess()}.")
+        # print(self.user_guess)
+        if self.user_guess == self.answer:
+            return f"Your last guess {self.user_guess} was {self.guess(self.user_guess)}."
+        else:
+            return f"Your last guess {self.user_guess} was {self.guess(self.user_guess)}."
 
 game = GuessingGame(10)
-print(game.guess(2))
+print(game.guess(4))
+print(game.solved())
